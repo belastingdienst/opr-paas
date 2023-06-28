7195428d7d88de4d11dddf23895df0a68902a532
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2023-06-28 20:21:22 +0200

    Also adding LDAP annotations

--- api/v1alpha1/paas_types_test.go
+++ api/v1alpha1/paas_types_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	group = v1alpha1.PaasGroups{
+	groups = v1alpha1.PaasGroups{
 		"cn=test1": v1alpha1.PaasGroup{
 			Query: "CN=test2,OU=org_unit,DC=example,DC=org",
 			Users: []string{"usr1", "usr2"},
@@ -22,13 +22,13 @@ var (
 )
 
 func TestPaasGroups_NameFromQuery(t *testing.T) {
-	assert.Equal(t, "test2", group.NameFromQuery("cn=test1"))
-	assert.Equal(t, "", group.NameFromQuery("cn=test123"))
-	assert.Equal(t, "test4", group.NameFromQuery("cn=test3"))
+	assert.Equal(t, "test2", groups.Key2Name("cn=test1"))
+	assert.Equal(t, "", groups.Key2Name("cn=test123"))
+	assert.Equal(t, "test4", groups.Key2Name("cn=test3"))
 }
 
 func TestPaasGroups_LdapQueries(t *testing.T) {
-	ldapGroups := group.LdapQueries()
+	ldapGroups := groups.LdapQueries()
 	sort.Strings(ldapGroups)
 	assert.Equal(t, 2, len(ldapGroups))
 	assert.Equal(t, "CN=test2,OU=org_unit,DC=example,DC=org", ldapGroups[0])
