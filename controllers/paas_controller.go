ec017c346a48029914791146d0bb02eab2dfdfbb
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2023-08-23 10:52:41 +0200

    rolebinding controller

--- controllers/paas_controller.go
+++ controllers/paas_controller.go
@@ -128,6 +128,17 @@ func (r *PaasReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 		}
 	}
 
+	logger.Info("Creating RoleBindings for PAAS object ")
+	// Create namespaces if needed
+	for _, rb := range r.BackendEnabledRoleBindings(ctx, paas) {
+		if err := r.EnsureAdminRoleBinding(ctx, rb); err != nil {
+			logger.Error(err, fmt.Sprintf("Failure while creating rolebinding %s/%s",
+				rb.ObjectMeta.Namespace,
+				rb.ObjectMeta.Name))
+			return ctrl.Result{}, err
+		}
+	}
+
 	for _, name := range r.BackendDisabledNamespaces(ctx, paas) {
 		logger.Info("Creating namespace " + name + " for PAAS object ")
 		if err := r.FinalizeNamespace(ctx, paas, name); err != nil {
