2ef2bb857768c85ccd3162adc30e4d60ccf5dda6
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2024-02-28 15:18:18 +0100

    apiGroups "" voor configmaps enzo

--- controllers/paas_controller.go
+++ controllers/paas_controller.go
@@ -36,7 +36,7 @@ type PaasReconciler struct {
 //+kubebuilder:rbac:groups=quota.openshift.io,resources=clusterresourcequotas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=user.openshift.io,resources=groups,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=argoproj.io,resources=argocds;applicationsets;applications;appprojects,verbs=create;delete
-//+kubebuilder:rbac:groups=,resources=secrets;configmaps;namespaces,verbs=create;delete;get;list;patch;update;watch
+//+kubebuilder:rbac:groups=core,resources=secrets;configmaps;namespaces,verbs=create;delete;get;list;patch;update;watch
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=create;delete;get;list;patch;update;watch;escallate
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=bind,resourceNames=admin
 
