9487334b62d3ecebe1abc5d296b566b8ad3fb06d
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2024-03-11 00:09:06 +0100

    Adding cobra and viper for cli experience

--- internal/crypt/main.go
+++ internal/crypt/main.go
@@ -19,60 +19,64 @@ func hashedKey(key []byte) []byte {
 	return hasher.Sum(nil)[:16]
 }
 
-func encrypt(publicKey string, paasName string, data []byte) {
+func encrypt(publicKey string, paasName string, data []byte) error {
 	c := NewCrypt("", publicKey, paasName)
 	if encrypted, err := c.Encrypt(data); err != nil {
-		panic(fmt.Sprintf("Failed to encrypt: %e", err))
+		return fmt.Errorf("failed to encrypt: %e", err)
 	} else {
 		fmt.Println(encrypted)
 	}
+	return nil
 }
 
-func DecryptFromStdin(privateKey string, paasName string) {
+func DecryptFromStdin(privateKey string, paasName string) error {
 	if data, err := io.ReadAll(os.Stdin); err != nil {
-		panic(err)
+		return err
 	} else {
 		c := NewCrypt(privateKey, "", paasName)
 		if encrypted, err := c.Decrypt(string(data)); err != nil {
-			panic(fmt.Sprintf("Failed to decrypt: %e", err))
+			return fmt.Errorf("failed to decrypt: %e", err)
 		} else {
 			fmt.Println(string(encrypted))
+			return nil
 		}
 	}
 }
 
-func EncryptFromStdin(publicKey string, paasName string) {
+func EncryptFromStdin(publicKey string, paasName string) error {
 	if data, err := io.ReadAll(os.Stdin); err != nil {
-		panic(err)
+		return err
 	} else {
-		encrypt(publicKey, paasName, data)
+		return encrypt(publicKey, paasName, data)
 	}
 }
 
-func EncryptFile(publicKey string, paasName string, path string) {
+func EncryptFile(publicKey string, paasName string, path string) error {
 	if data, err := os.ReadFile(path); err != nil {
-		panic(err)
+		return err
 	} else {
-		encrypt(publicKey, paasName, data)
+		return encrypt(publicKey, paasName, data)
 	}
 }
 
-func newFile(envVar string) string {
-	if path := os.Getenv(envVar); path != "" {
-		return path
-	}
-	if f, err := os.CreateTemp("", "paas"); err != nil {
-		panic(fmt.Sprintf("%s not specified and failed to create temp file: %e", envVar, err))
+func GenerateKeyPair(privateKey string, publicKey string) error {
+	if privateKey != "" {
+
+	} else if f, err := os.CreateTemp("", "paas"); err != nil {
+		return fmt.Errorf("privateKey not specified and failed to create temp file: %e", err)
 	} else {
-		return f.Name()
+		privateKey = f.Name()
 	}
-}
 
-func GenerateKeyPair() {
-	priv := newFile("PAAS_PRIVATE_KEY_PATH")
-	pub := newFile("PAAS_PUBLIC_KEY_PATH")
+	if publicKey != "" {
 
-	if _, err := NewCrypt(priv, pub, "").GenerateCrypt(); err != nil {
-		panic(fmt.Sprintf("Failed to generate new key pair: %e", err))
+	} else if f, err := os.CreateTemp("", "paas"); err != nil {
+		return fmt.Errorf("privateKey not specified and failed to create temp file: %e", err)
+	} else {
+		publicKey = f.Name()
 	}
+	if _, err := NewCrypt(privateKey, publicKey, "").GenerateCrypt(); err != nil {
+		return fmt.Errorf("failed to generate new key pair: %e", err)
+	}
+	return nil
 }
