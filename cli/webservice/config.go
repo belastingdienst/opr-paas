41ce4b43721dae51d2556086508954e5872520a2
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2024-03-13 17:01:02 +0100

    SonarQube Reduce Cognitive Complexity 1

--- cli/webservice/config.go
+++ cli/webservice/config.go
@@ -17,11 +17,11 @@ import (
 )
 
 const (
-	publicEnv         = "PAAS_PUBLIC_KEY_PATH"
-	defaultPublicPath = "/secrets/paas/publicKey"
-	endpointEnv       = "PAAS_ENDPOINT"
-	defaultEndpoint   = ":8080"
-	adminApiKey       = "PAAS_ADMIN_API_KEY"
+	publicEnv           = "PAAS_PUBLIC_KEY_PATH"
+	defaultPublicPath   = "/secrets/paas/publicKey"
+	endpointEnv         = "PAAS_ENDPOINT"
+	defaultEndpointPort = 8080
+	adminApiKey         = "PAAS_ADMIN_API_KEY"
 )
 
 type WSConfig struct {
@@ -40,41 +40,40 @@ func randStringBytes(n int) string {
 	return string(b)
 }
 
+func formatEndpoint(endpoint string) string {
+	if endpoint == "" {
+		return fmt.Sprintf(":%d", defaultEndpointPort)
+	}
+	if strings.Contains(endpoint, ":") {
+		parts := strings.Split(endpoint, ":")
+		host := parts[0]
+		if len(host) > 63 {
+			panic(fmt.Errorf("invalid hostname %s longer than 63 characters", host))
+		} else if match, err := regexp.MatchString(`[^0-9.a-zA-Z-:]`, host); err != nil {
+			panic("invalid regular expression for hostname")
+		} else if match {
+			panic(fmt.Errorf("invalid hostname %s in endpoint config", host))
+		}
+		port := parts[1]
+		if port == "" {
+			port = fmt.Sprintf("%d", defaultEndpointPort)
+		} else if portNum, err := strconv.Atoi(port); err != nil {
+			panic(fmt.Errorf("invalid hostname %s in endpoint config", port))
+		} else if portNum < 0 || portNum > 65353 {
+			panic(fmt.Errorf("invalid port %s not in valid RFC range (0-65363)", port))
+		}
+		return fmt.Sprintf("%s:%s", host, port)
+	}
+	return fmt.Sprintf("%s:%d", endpoint, defaultEndpointPort)
+}
+
 func NewWSConfig() WSConfig {
 	var config WSConfig
 	config.PublicKeyPath = os.Getenv(publicEnv)
 	if config.PublicKeyPath == "" {
 		config.PublicKeyPath = defaultPublicPath
 	}
-	config.Endpoint = os.Getenv(endpointEnv)
-	if config.Endpoint == "" {
-		config.Endpoint = defaultEndpoint
-	} else if strings.Contains(config.Endpoint, ":") {
-		parts := strings.Split(config.Endpoint, ":")
-		host := parts[0]
-		if len(host) > 63 {
-			panic(fmt.Errorf("invalid hostname %s longer than 63 characters", host))
-		} else {
-			if match, err := regexp.MatchString(`[^0-9.a-zA-Z-:]`, host); err != nil {
-				panic("invalid regular expression for hostname")
-			} else if match {
-				panic(fmt.Errorf("invalid hostname %s in endpoint config", host))
-			}
-		}
-		port := parts[1]
-		if port == "" {
-			host = "8080"
-		} else {
-			if portNum, err := strconv.Atoi(port); err != nil {
-				panic(fmt.Errorf("invalid hostname %s in endpoint config", port))
-			} else if portNum < 0 || portNum > 65353 {
-				panic(fmt.Errorf("invalid port %s not in valid RFC range (0-65363)", port))
-			}
-		}
-		config.Endpoint = fmt.Sprintf("%s:%s", host, port)
-	} else {
-		config.Endpoint = fmt.Sprintf("%s:8080", config.Endpoint)
-	}
+	config.Endpoint = formatEndpoint(os.Getenv(endpointEnv))
 	config.AdminApiKey = os.Getenv(adminApiKey)
 	if config.AdminApiKey == "" {
 		config.AdminApiKey = randStringBytes(64)
