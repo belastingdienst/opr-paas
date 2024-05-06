1a3b841567d43411bf6d3cd4ef01e03885709b2a
hikarukin <156204784+hikarukin@users.noreply.github.com>
2024-05-06 17:14:36 +0200

    Add test stubs

---
+++ cli/crypttool/paasFile_test.go
@@ -0,0 +1,21 @@
+/*
+Copyright 2024, Tax Administration of The Netherlands.
+Licensed under the EUPL 1.2.
+See LICENSE.md for details.
+*/
+
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_writeFormattedFile(t *testing.T) {
+	assert.Fail(t, "Not Implemented")
+}
+
+func Test_hashData(t *testing.T) {
+	assert.Fail(t, "Not Implemented")
+}
