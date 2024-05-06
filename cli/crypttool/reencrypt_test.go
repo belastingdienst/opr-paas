365d41afc0e11f686f8849aa4b2a65c526152ac7
hikarukin <156204784+hikarukin@users.noreply.github.com>
2024-05-06 17:14:36 +0200

    Add test stubs

---
+++ cli/crypttool/reencrypt_test.go
@@ -0,0 +1,17 @@
+/*
+Copyright 2024, Tax Administration of The Netherlands.
+Licensed under the EUPL 1.2.
+See LICENSE.md for details.
+*/
+
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_reencryptSecret(t *testing.T) {
+	assert.Fail(t, "Not Implemented")
+}
