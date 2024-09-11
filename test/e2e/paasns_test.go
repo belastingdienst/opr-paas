7a6e126d01ee34517e2abc03dcf24a6812cfef5c
taxing4u <147594874+taxing4u@users.noreply.github.com>
2024-09-11 16:56:12 +0200

    cleanup

--- test/e2e/paasns_test.go
+++ test/e2e/paasns_test.go
@@ -2,7 +2,6 @@ package e2e
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
@@ -45,13 +44,14 @@ func TestPaasNS(t *testing.T) {
 		t,
 		features.New("PaasNS").
 			// Setup(createPaasFn(paasWithQuota, paasSpec)).
-			Assess("paasns creation with reference to non-existing paas", assertPaasNSWithoutPaas).
-			// Assess("is created", assertPaasNSCreated).
+			Assess("PaasNS creation without linked Paas", assertPaasNSCreatedWithoutPaas).
+			// Assess("PaasNS creation with linked Paas", assertPaasNSCreated).
+			// Assess("PaasNS deletion", assertPaasNSDeletion).
 			Feature(),
 	)
 }
 
-func assertPaasNSWithoutPaas(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+func assertPaasNSCreatedWithoutPaas(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 	paasNs := &api.PaasNS{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      paasNsName,
@@ -65,26 +65,18 @@ func assertPaasNSWithoutPaas(ctx context.Context, t *testing.T, cfg *envconf.Con
 		t.Fatal(err)
 	}
 
-	// fetch paas but expect it to error because it shouldn't have been created just because we referenced it
-	_, errPaas := getPaas(ctx, t, cfg)
-
-	// referenced paas still should not exist
-	assert.Error(t, errPaas)
-
+	// give cluster some time to create resources otherwise asserts fire too soon
+	// possibly better solved by seperating into Setup() and Assess() steps (refactor)
+	// or we could maybe use sigs.k8s.io/e2e-framework/klient/wait
 	waitForOperator()
 
+	// check that the referenced paas hasn't been created on-the-fly
+	_, errPaas := getPaas(ctx, t, cfg)
+	assert.Error(t, errPaas)
+
+	// check that the paasns has been created but also contains an error status message
 	fetchedPaasNS := getPaasNS(ctx, t, cfg)
-
-	// assert paasns status message
-	fmt.Println("----------------")
-	fmt.Println(fetchedPaasNS)
-	fmt.Println(fetchedPaasNS.Name)
-	fmt.Println(fetchedPaasNS.Status.Messages) // Error message disappeared but was there last week. Possibly a timing issue
-
-	// checking for vals...
 	var errMsg = fetchedPaasNS.Status.Messages[0]
-	fmt.Println(errMsg)
-	// assert.Equal(t, paasNsName, &paasns.Name)
 	assert.Contains(t, errMsg, "cannot find PaaS")
 
 	return ctx
