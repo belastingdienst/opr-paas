df91a8fd3b74f803ab710d5f8baa178ac8ca09c0
paradisepalms <172507584+paradisepalms@users.noreply.github.com>
2024-09-18 16:46:32 +0200

    sync with latest changes

--- test/e2e/util.go
+++ test/e2e/util.go
@@ -11,18 +11,19 @@ import (
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/types"
 )
 
-// Duration to pause after PaaS creation to wait for reconciliation.
+// Duration to pause after Paas creation to wait for reconciliation.
 const waitForOperatorDuration = 1 * time.Second
 
 func waitForOperator() {
 	time.Sleep(waitForOperatorDuration)
 }
 
-// createPaasFn accepts a PaaS spec object and a name and creates the PaaS resource.
+// createPaasFn accepts a Paas spec object and a name and creates the Paas resource.
 func createPaasFn(name string, paasSpec api.PaasSpec) types.StepFunc {
 	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 		paas := &api.Paas{
@@ -31,7 +32,7 @@ func createPaasFn(name string, paasSpec api.PaasSpec) types.StepFunc {
 		}
 
 		if err := cfg.Client().Resources().Create(ctx, paas); err != nil {
-			t.Fatalf("Failed to create PaaS resource: %v", err)
+			t.Fatalf("Failed to create Paas resource: %v", err)
 		}
 
 		waitForOperator()
@@ -40,7 +41,7 @@ func createPaasFn(name string, paasSpec api.PaasSpec) types.StepFunc {
 	}
 }
 
-// teardownPaasFn deletes the PaaS if it still exists (e.g. if deleting the PaaS is not part of the test steps, or if an
+// teardownPaasFn deletes the Paas if it still exists (e.g. if deleting the Paas is not part of the test steps, or if an
 // earlier assertion failed causing the deletion step to be skipped).
 // Can be called as `.Teardown(teardownPaasFn("paas-name"))`
 func teardownPaasFn(paasName string) types.StepFunc {
@@ -55,26 +56,39 @@ func teardownPaasFn(paasName string) types.StepFunc {
 	}
 }
 
-// getPaas retrieves the PaaS with the associated name.
-func getPaas(ctx context.Context, name string, t *testing.T, cfg *envconf.Config) api.Paas {
-	var paas api.Paas
-
-	if err := cfg.Client().Resources().Get(ctx, name, cfg.Namespace(), &paas); err != nil {
-		t.Fatalf("Failed to retrieve PaaS: %v", err)
-	}
+// getPaas retrieves the Paas with the associated name.
+func getPaas(ctx context.Context, name string, t *testing.T, cfg *envconf.Config) *api.Paas {
+	paas := &api.Paas{}
+	getOrFail(ctx, name, cfg.Namespace(), paas, t, cfg)
 
 	return paas
 }
 
-// deletePaas deletes the PaaS with the associated name.
+// deletePaas deletes the Paas with the associated name.
 func deletePaas(ctx context.Context, name string, t *testing.T, cfg *envconf.Config) {
 	paas := &api.Paas{ObjectMeta: metav1.ObjectMeta{Name: name}}
 
 	if err := cfg.Client().Resources().Delete(ctx, paas); err != nil {
-		t.Fatalf("Failed to delete PaaS: %v", err)
+		t.Fatalf("Failed to delete Paas: %v", err)
 	}
 
 	waitForOperator()
+}
+
+func getOrFail[T k8s.Object](ctx context.Context, name string, namespace string, obj T, t *testing.T, cfg *envconf.Config) T {
+	if err := cfg.Client().Resources().Get(ctx, name, namespace, obj); err != nil {
+		t.Fatalf("Failed to get resource %s: %v", name, err)
+	}
+
+	return obj
+}
+
+func listOrFail[L k8s.ObjectList](ctx context.Context, namespace string, obj L, t *testing.T, cfg *envconf.Config) L {
+	if err := cfg.Client().Resources(namespace).List(ctx, obj); err != nil {
+		t.Fatalf("Failed to get resource list: %v", err)
+	}
+
+	return obj
 }
 
 func getNamespace(ctx context.Context, t *testing.T, cfg *envconf.Config, name string) corev1.Namespace {
