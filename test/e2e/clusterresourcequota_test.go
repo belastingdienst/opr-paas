27a3059c917d75155c6dcf624d93e95a897ba9d8
AxiomaticFixedChimpanzee <179703613+AxiomaticFixedChimpanzee@users.noreply.github.com>
2024-09-18 16:14:02 +0200

    Test groups with users (#36)
    * Add teardown step to CRQ e2e test
    * Define test for user groups
    * Fix PaasNS API version references

--- test/e2e/clusterresourcequota_test.go
+++ test/e2e/clusterresourcequota_test.go
@@ -10,7 +10,6 @@ import (
 	quotav1 "github.com/openshift/api/quota/v1"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -32,7 +31,8 @@ func TestClusterResourceQuota(t *testing.T) {
 			Setup(createPaasFn(paasWithQuota, paasSpec)).
 			Assess("is created", assertCRQCreated).
 			Assess("is updated", assertCRQUpdated).
-			Assess("is deleted when PaaS is deleted", assertCRQDeleted).
+			Assess("is deleted when Paas is deleted", assertCRQDeleted).
+			Teardown(teardownPaasFn(paasWithQuota)).
 			Feature(),
 	)
 }
@@ -40,7 +40,11 @@ func TestClusterResourceQuota(t *testing.T) {
 func assertCRQCreated(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 	crq := getCRQ(ctx, t, cfg)
 
+	// ClusterResourceQuota is created with the same name as the Paas
 	assert.Equal(t, paasWithQuota, crq.Name)
+	// The label selector matches the Paas name
+	assert.Equal(t, paasWithQuota, crq.Spec.Selector.LabelSelector.MatchLabels["q.lbl"])
+	// The quota size matches those passed in the Paas spec
 	assert.Equal(t, resource.MustParse("200m"), *crq.Spec.Quota.Hard.Cpu())
 	assert.Equal(t, resource.MustParse("256Mi"), *crq.Spec.Quota.Hard.Memory())
 
@@ -48,19 +52,15 @@ func assertCRQCreated(ctx context.Context, t *testing.T, cfg *envconf.Config) co
 }
 
 func assertCRQUpdated(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-	var paas api.Paas
-
-	if err := cfg.Client().Resources().Get(ctx, paasWithQuota, cfg.Namespace(), &paas); err != nil {
-		t.Fatalf("Failed to retrieve PaaS: %v", err)
-	}
+	paas := getPaas(ctx, paasWithQuota, t, cfg)
 
 	paas.Spec.Quota = quota.NewQuota(map[string]string{
 		"cpu":    "100m",
 		"memory": "128Mi",
 	})
 
-	if err := cfg.Client().Resources().Update(ctx, &paas); err != nil {
-		t.Fatalf("Failed to update PaaS resource: %v", err)
+	if err := cfg.Client().Resources().Update(ctx, paas); err != nil {
+		t.Fatalf("Failed to update Paas resource: %v", err)
 	}
 
 	waitForOperator()
@@ -74,31 +74,14 @@ func assertCRQUpdated(ctx context.Context, t *testing.T, cfg *envconf.Config) co
 }
 
 func assertCRQDeleted(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-	paas := &api.Paas{ObjectMeta: metav1.ObjectMeta{Name: paasWithQuota}}
+	deletePaas(ctx, paasWithQuota, t, cfg)
+	crqs := listOrFail(ctx, "", &quotav1.ClusterResourceQuotaList{}, t, cfg)
 
-	if err := cfg.Client().Resources().Delete(ctx, paas); err != nil {
-		t.Fatalf("Failed to delete PaaS: %v", err)
-	}
-
-	waitForOperator()
-
-	var crqs quotav1.ClusterResourceQuotaList
-
-	if err := cfg.Client().Resources().List(ctx, &crqs); err != nil {
-		t.Fatalf("Failed to retrieve list of ClusterResourceQuotas: %v", err)
-	}
-
-	assert.Empty(t, 0, len(crqs.Items))
+	assert.Empty(t, crqs.Items)
 
 	return ctx
 }
 
-func getCRQ(ctx context.Context, t *testing.T, cfg *envconf.Config) quotav1.ClusterResourceQuota {
-	var crq quotav1.ClusterResourceQuota
-
-	if err := cfg.Client().Resources().Get(ctx, paasWithQuota, cfg.Namespace(), &crq); err != nil {
-		t.Fatalf("Failed to retrieve ClusterResourceQuota: %v", err)
-	}
-
-	return crq
+func getCRQ(ctx context.Context, t *testing.T, cfg *envconf.Config) *quotav1.ClusterResourceQuota {
+	return getOrFail(ctx, paasWithQuota, cfg.Namespace(), &quotav1.ClusterResourceQuota{}, t, cfg)
 }
--- test/e2e/util.go
+++ test/e2e/util.go
@@ -6,19 +6,21 @@ import (
 	"time"
 
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/types"
 )
 
-// Duration to pause after PaaS creation to wait for reconciliation.
+// Duration to pause after Paas creation to wait for reconciliation.
 const waitForOperatorDuration = 1 * time.Second
 
 func waitForOperator() {
 	time.Sleep(waitForOperatorDuration)
 }
 
-// createPaasFn accepts a PaaS spec object and a name and creates the PaaS resource.
+// createPaasFn accepts a Paas spec object and a name and creates the Paas resource.
 func createPaasFn(name string, paasSpec api.PaasSpec) types.StepFunc {
 	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 		paas := &api.Paas{
@@ -27,11 +29,61 @@ func createPaasFn(name string, paasSpec api.PaasSpec) types.StepFunc {
 		}
 
 		if err := cfg.Client().Resources().Create(ctx, paas); err != nil {
-			t.Fatalf("Failed to create PaaS resource: %v", err)
+			t.Fatalf("Failed to create Paas resource: %v", err)
 		}
 
 		waitForOperator()
 
 		return ctx
 	}
+}
+
+// teardownPaasFn deletes the Paas if it still exists (e.g. if deleting the Paas is not part of the test steps, or if an
+// earlier assertion failed causing the deletion step to be skipped).
+// Can be called as `.Teardown(teardownPaasFn("paas-name"))`
+func teardownPaasFn(paasName string) types.StepFunc {
+	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		paas := &api.Paas{ObjectMeta: metav1.ObjectMeta{Name: paasName}}
+
+		if cfg.Client().Resources().Delete(ctx, paas) == nil {
+			t.Logf("Paas %s deleted", paasName)
+		}
+
+		return ctx
+	}
+}
+
+// getPaas retrieves the Paas with the associated name.
+func getPaas(ctx context.Context, name string, t *testing.T, cfg *envconf.Config) *api.Paas {
+	paas := &api.Paas{}
+	getOrFail(ctx, name, cfg.Namespace(), paas, t, cfg)
+
+	return paas
+}
+
+// deletePaas deletes the Paas with the associated name.
+func deletePaas(ctx context.Context, name string, t *testing.T, cfg *envconf.Config) {
+	paas := &api.Paas{ObjectMeta: metav1.ObjectMeta{Name: name}}
+
+	if err := cfg.Client().Resources().Delete(ctx, paas); err != nil {
+		t.Fatalf("Failed to delete Paas: %v", err)
+	}
+
+	waitForOperator()
+}
+
+func getOrFail[T k8s.Object](ctx context.Context, name string, namespace string, obj T, t *testing.T, cfg *envconf.Config) T {
+	if err := cfg.Client().Resources().Get(ctx, name, namespace, obj); err != nil {
+		t.Fatalf("Failed to get resource %s: %v", name, err)
+	}
+
+	return obj
+}
+
+func listOrFail[L k8s.ObjectList](ctx context.Context, namespace string, obj L, t *testing.T, cfg *envconf.Config) L {
+	if err := cfg.Client().Resources(namespace).List(ctx, obj); err != nil {
+		t.Fatalf("Failed to get resource list: %v", err)
+	}
+
+	return obj
 }
