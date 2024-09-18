7efea484820628c08f407479190b66407cb03ea7
taxing4u <147594874+taxing4u@users.noreply.github.com>
2024-09-18 10:20:14 +0200

    assertPaasNScreated

--- test/e2e/paasns_test.go
+++ test/e2e/paasns_test.go
@@ -5,12 +5,14 @@ import (
 	"testing"
 
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
+	"github.com/belastingdienst/opr-paas/internal/quota"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
 
+const paasNamespace = "paas-e2e"
 const paasNsName = "a-paasns"
 const paasName = "a-paas"
 
@@ -31,6 +33,7 @@ const paasName = "a-paas"
 // 		Spec: api.PaasNSSpec{Paas: paasName}, // this paas does not exist but we do reference it
 // 	}
 
+// Should we not use the e2e package setup-feature-teardown steps?
 // 	testenv.Test(
 // 		t,
 // 		features.New("PaasNS creation without linked Paas").
@@ -45,7 +48,7 @@ func TestPaasNS(t *testing.T) {
 		features.New("PaasNS").
 			// Setup(createPaasFn(paasWithQuota, paasSpec)).
 			Assess("PaasNS creation without linked Paas", assertPaasNSCreatedWithoutPaas).
-			// Assess("PaasNS creation with linked Paas", assertPaasNSCreated).
+			Assess("PaasNS creation with linked Paas", assertPaasNSCreated).
 			// Assess("PaasNS deletion", assertPaasNSDeletion).
 			Feature(),
 	)
@@ -83,9 +86,48 @@ func assertPaasNSCreatedWithoutPaas(ctx context.Context, t *testing.T, cfg *envc
 	return ctx
 }
 
-// func assertPaasNSCreated(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+func assertPaasNSCreated(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+	// var quota = api.PaasSpec.Quota
 
-// }
+	// setup: create paas to link to
+	paas := &api.Paas{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      paasName,
+			Namespace: paasNamespace,
+		},
+		Spec: api.PaasSpec{
+			Quota: quota.NewQuota(map[string]string{
+				"cpu":    "2",
+				"memory": "2Gi",
+			}),
+		},
+	}
+
+	createPaas(ctx, t, cfg, *paas)
+	waitForOperator()
+
+	paasNs := &api.PaasNS{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      paasNsName,
+			Namespace: paasNamespace,
+		},
+		Spec: api.PaasNSSpec{Paas: paasName},
+	}
+
+	createPaasNS(ctx, t, cfg, *paasNs)
+	waitForOperator()
+
+	// check that the paasns has been created and is linked to the correct paas
+	fetchedPaasNS := getPaasNS(ctx, t, cfg)
+	var linkedPaas = fetchedPaasNS.Spec.Paas
+	assert.Equal(t, linkedPaas, paasName)
+
+	// cleanup
+	deletePaas(ctx, t, cfg, *paas)
+	deletePaasNS(ctx, t, cfg, *paasNs)
+
+	return ctx
+}
 
 func createPaasNS(ctx context.Context, t *testing.T, cfg *envconf.Config, paasns api.PaasNS) api.PaasNS {
 
@@ -112,7 +154,22 @@ func deletePaasNS(ctx context.Context, t *testing.T, cfg *envconf.Config, paasns
 	}
 }
 
+func createPaas(ctx context.Context, t *testing.T, cfg *envconf.Config, paas api.Paas) api.Paas {
+
+	if err := cfg.Client().Resources().Create(ctx, &paas); err != nil {
+		t.Fatalf("Failed to create Paas: %v", err)
+	}
+
+	return paas
+}
+
 func getPaas(ctx context.Context, t *testing.T, cfg *envconf.Config) (paas api.Paas, err error) {
 	err = cfg.Client().Resources().Get(ctx, paasNsName, cfg.Namespace(), &paas)
 	return paas, err
+}
+
+func deletePaas(ctx context.Context, t *testing.T, cfg *envconf.Config, paas api.Paas) {
+	if err := cfg.Client().Resources().Delete(ctx, &paas); err != nil {
+		t.Fatalf("Failed to delete Paas: %v", err)
+	}
 }
