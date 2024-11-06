b32690de42314242b20012545ff96c580e5cfe9a
portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
2024-11-06 10:03:22 +0100

    Fix missing ArgoCD patch permissions.
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>

--- internal/controller/paas_controller.go
+++ internal/controller/paas_controller.go
@@ -50,7 +50,7 @@ type Reconciler interface {
 
 //+kubebuilder:rbac:groups=quota.openshift.io,resources=clusterresourcequotas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=user.openshift.io,resources=groups,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=argoproj.io,resources=argocds;applicationsets;applications;appprojects,verbs=create;delete;list;watch;update
+//+kubebuilder:rbac:groups=argoproj.io,resources=argocds;applicationsets;applications;appprojects,verbs=create;delete;list;patch;watch;update
 //+kubebuilder:rbac:groups=core,resources=secrets;configmaps;namespaces,verbs=create;delete;get;list;patch;update;watch
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings;clusterrolebindings,verbs=create;delete;get;list;patch;update;watch;escallate
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=bind,resourceNames=admin
