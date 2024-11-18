15ea3cfa948263ebe64d48033281170d97343e98
Benoit <77777295+benoitschipper@users.noreply.github.com>
2024-11-18 11:31:10 +0100

    Update paasns_controller.go
    Remove from paasns_controller scope
    
    Signed-off-by: Benoit <77777295+benoitschipper@users.noreply.github.com>

--- internal/controller/paasns_controller.go
+++ internal/controller/paasns_controller.go
@@ -41,7 +41,6 @@ func (pr PaasNSReconciler) GetScheme() *runtime.Scheme {
 //+kubebuilder:rbac:groups=cpet.belastingdienst.nl,resources=paasns,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cpet.belastingdienst.nl,resources=paasns/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cpet.belastingdienst.nl,resources=paasns/finalizers,verbs=update
-//+kubebuilder:rbac:groups=argoproj.io,resources=applicationsets,verbs=get;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
