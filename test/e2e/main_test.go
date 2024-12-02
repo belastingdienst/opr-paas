3c7f3209f30b1bbfbfbb44b0cae28569127d8503
hikarukin <156204784+hikarukin@users.noreply.github.com>
2024-12-02 21:36:18 +0100

    fix CICD by using env var to control secrets path
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -14,18 +14,19 @@ import (
 
 	argoprojlabsv1beta1 "github.com/belastingdienst/opr-paas/internal/stubs/argoproj-labs/v1beta1"
 	argoprojv1alpha1 "github.com/belastingdienst/opr-paas/internal/stubs/argoproj/v1alpha1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/belastingdienst/opr-paas/api/v1alpha1"
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
 
 	quotav1 "github.com/openshift/api/quota/v1"
 	userv1 "github.com/openshift/api/user/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
 	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
-	"sigs.k8s.io/e2e-framework/support/utils"
 
 	"sigs.k8s.io/e2e-framework/klient/conf"
 	"sigs.k8s.io/e2e-framework/pkg/env"
@@ -58,24 +59,35 @@ func TestMain(m *testing.M) {
 
 	testenv.Setup(
 		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-			// install PaasConfig instance
-			if p := utils.RunCommand(
-				fmt.Sprintf("kubectl apply -f %s", "../../manifests/config/example-paasconfig.yaml"),
-			); p.Err() != nil {
-				return ctx, p.Err()
-			}
-
-			paasconfig := &v1alpha1.PaasConfig{
-				ObjectMeta: v1.ObjectMeta{
+			paasconfig := &api.PaasConfig{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "paas-config",
 				},
 			}
+
 			waitUntilPaasConfigExists := conditions.New(cfg.Client().Resources()).ResourceMatch(paasconfig, func(obj k8s.Object) bool {
 				return obj.(*api.PaasConfig).Name == paasconfig.Name
 			})
 
 			if err := waitForDefaultOpts(ctx, waitUntilPaasConfigExists); err != nil {
 				return nil, err
+			}
+
+			if err := cfg.Client().Resources().Get(ctx, "paas-config", "", paasconfig); err != nil {
+				return nil, err
+			}
+
+			// Patch decryptKeyPaths for testing when SECRETS_PATH env is set
+			if secretsPath := os.Getenv("SECRETS_PATH"); secretsPath != "" {
+				payload := fmt.Sprintf(`{"spec": {"decryptKeyPaths": ["%s"]}}`, secretsPath)
+				patch := k8s.Patch{
+					PatchType: types.MergePatchType,
+					Data:      []byte(payload),
+				}
+
+				if err := cfg.Client().Resources().Patch(ctx, paasconfig, patch); err != nil {
+					return nil, err
+				}
 			}
 
 			return ctx, nil
