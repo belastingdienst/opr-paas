91231373b0648ba6cd2d635eeec996e822b5cf0e
portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
2025-01-21 11:29:58 +0100

    chore: Refine group CRD descriptions (#300)
    * * Adds / updates descriptions related to groups in Paas(Ns) types.
    * Updates crds accordingly.
    
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
    
    * * Adds / updates descriptions related to groups in Paas(Ns) types.
    * Updates crds accordingly.
    
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
    
    * *x
    
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
    
    * Adds some more details.
    
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>

--- api/v1alpha1/paas_types.go
+++ api/v1alpha1/paas_types.go
@@ -38,6 +38,8 @@ type PaasSpec struct {
 	// +kubebuilder:validation:Required
 	Requestor string `json:"requestor"`
 
+	// Groups define k8s groups, based on an LDAP query or a list of LDAP users, which get access to the namespaces
+	// belonging to this Paas. Per group, RBAC roles can be defined.
 	// +kubebuilder:validation:Optional
 	Groups PaasGroups `json:"groups"`
 
@@ -133,10 +135,19 @@ func (p Paas) extraNamespaces() (ns map[string]bool) {
 }
 
 type PaasGroup struct {
+	// A fully qualified LDAP query which will be used by the Group Sync Operator to sync users to the defined group.
+	//
+	// When set in combination with `users`, the Group Sync Operator will overwrite the manually assigned users.
+	// Therefore, this field is mutually exclusive with `group.users`.
 	// +kubebuilder:validation:Optional
 	Query string `json:"query"`
+	// A list of LDAP users which are added to the defined group.
+	//
+	// When set in combination with `users`, the Group Sync Operator will overwrite the manually assigned users.
+	// Therefore, this field is mutually exclusive with `group.query`.
 	// +kubebuilder:validation:Optional
 	Users []string `json:"users"`
+	// List of roles, as defined in the `PaasConfig` which the users in this group get assigned via a rolebinding.
 	// +kubebuilder:validation:Optional
 	Roles []string `json:"roles"`
 }
@@ -286,7 +297,7 @@ type PaasCapability struct {
 	// Custom fields to configure this specific Capability
 	// +kubebuilder:validation:Optional
 	CustomFields map[string]string `json:"custom_fields"`
-	// This project has it's own ClusterResourceQuota settings
+	// This project has its own ClusterResourceQuota settings
 	// +kubebuilder:validation:Optional
 	Quota paas_quota.Quota `json:"quota"`
 	// You can add ssh keys (which is a type of secret) for capability to use for access to bitBucket
