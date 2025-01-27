d2b39d20e1d9102e20717da01942d9f0eca8a58c
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-01-27 15:52:14 +0100

    Adding notifier unittest on files and on symlinks

--- internal/utils/notifier_test.go
+++ internal/utils/notifier_test.go
@@ -11,12 +11,12 @@ import (
 )
 
 const (
-	timeout = 1 * time.Millisecond
+	timeout = 10 * time.Millisecond
 )
 
-func Test_FileChanged(t *testing.T) {
+func Test_FolderChanged(t *testing.T) {
 	// Create folder
-	tmpDir, err := os.MkdirTemp("", "sampledir")
+	tmpDir, err := os.MkdirTemp("", "notifierFolderTest")
 	if err != nil {
 		panic(fmt.Errorf("unable to create temp dir: %w", err))
 	}
@@ -54,4 +54,79 @@ func Test_FileChanged(t *testing.T) {
 	}
 	time.Sleep(timeout)
 	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after adding file")
+}
+
+func Test_FileChanged(t *testing.T) {
+	// Create folder
+	tmpDir, err := os.MkdirTemp("", "notifierFileTest")
+	if err != nil {
+		panic(fmt.Errorf("unable to create temp dir: %w", err))
+	}
+
+	// add file
+	filePath := filepath.Join(tmpDir, "extra")
+	t.Logf("writing file %s", filePath)
+	if err := os.WriteFile(filePath, []byte("initial file data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to create extra temp file: %w", err))
+	}
+	fw := NewFileWatcher(filePath)
+	time.Sleep(timeout)
+	require.False(t, fw.WasTriggered(), "fileWatcher was not triggered after init")
+
+	t.Logf("writing file %s", filePath)
+	if err := os.WriteFile(filePath, []byte("other file data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to write to extra temp file: %w", err))
+	}
+	time.Sleep(timeout)
+	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after writing to file")
+
+	os.Remove(filePath)
+	if err := os.WriteFile(filePath, []byte("recreated file data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to write to extra temp file: %w", err))
+	}
+	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after removing file")
+
+	if err := os.WriteFile(filePath, []byte("recreated file data again"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to write to extra temp file: %w", err))
+	}
+	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after recreating file")
+}
+
+func Test_LinkChanged(t *testing.T) {
+	// Create folder
+	tmpDir, err := os.MkdirTemp("", "notifierSymlinkTest")
+	if err != nil {
+		panic(fmt.Errorf("unable to create temp dir: %w", err))
+	}
+
+	// add file
+	filePath := filepath.Join(tmpDir, "extra")
+	t.Logf("writing file %s", filePath)
+	if err := os.WriteFile(filePath, []byte("initial file data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to create extra temp file: %w", err))
+	}
+	symlinkPath := filepath.Join(tmpDir, "symlink")
+	os.Symlink(filePath, symlinkPath)
+
+	fw := NewFileWatcher(symlinkPath)
+	time.Sleep(timeout)
+	require.False(t, fw.WasTriggered(), "fileWatcher was not triggered after init")
+
+	t.Logf("writing to symlink %s", symlinkPath)
+	if err := os.WriteFile(symlinkPath, []byte("other symlink data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to write to symlink: %w", err))
+	}
+	time.Sleep(timeout)
+	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after writing to symlink")
+
+	os.Remove(symlinkPath)
+	time.Sleep(timeout)
+	os.Symlink(filePath, symlinkPath)
+	// !!! known behaviour. fsnotifier does not track symlinks themselves, but files they point to
+	require.False(t, fw.WasTriggered(), "fileWatcher is not triggered after removing symlink")
+
+	if err := os.WriteFile(filePath, []byte("recreated symlink data"), 0o600); err != nil {
+		panic(fmt.Errorf("unable to write to extra temp file: %w", err))
+	}
+	require.True(t, fw.WasTriggered(), "fileWatcher was triggered after recreating symlink")
 }
