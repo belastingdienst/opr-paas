f1035932f32e09911b97f8e9fdc6a5cf2d38bfea
AxiomaticFixedChimpanzee <179703613+AxiomaticFixedChimpanzee@users.noreply.github.com>
2025-02-05 16:19:24 +0100

    feat(paas_webhook): add capability validation in admission webhook (#332)
    * feat(paas_webhook): add capability validation
    
    * feat(paas_webhook): add capability validation on Paas update
    
    * test(e2e): fix cap6 test to pass admission hook
    
    * feat(paas_webhook): return list of field errors
    
    * feat(paas_webhook): return all invalid capabilities as error fields

--- test/e2e/capability-cap6_test.go
+++ test/e2e/capability-cap6_test.go
@@ -7,10 +7,11 @@ import (
 	api "github.com/belastingdienst/opr-paas/api/v1alpha1"
 	"github.com/belastingdienst/opr-paas/internal/quota"
 	argo "github.com/belastingdienst/opr-paas/internal/stubs/argoproj/v1alpha1"
-
 	quotav1 "github.com/openshift/api/quota/v1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	resourcev1 "k8s.io/apimachinery/pkg/api/resource"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -35,6 +36,24 @@ func TestCapabilityCap6(t *testing.T) {
 	testenv.Test(
 		t,
 		features.New("Capability 6").
+			Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				// Update the active PaasConfig to configure cap6
+				var paasConfig api.PaasConfig
+				err := cfg.Client().Resources().Get(ctx, "paas-config", "", &paasConfig)
+				require.NoError(t, err, "Failed to get PaasConfig")
+
+				paasConfig.Spec.Capabilities[cap6Name] = api.ConfigCapability{
+					AppSet: cap6ApplicationSet,
+					QuotaSettings: api.ConfigQuotaSettings{
+						DefQuota: map[corev1.ResourceName]resourcev1.Quantity{},
+					},
+				}
+
+				err = cfg.Client().Resources().Update(ctx, &paasConfig)
+				require.NoError(t, err, "Failed to update PaasConfig")
+
+				return ctx
+			}).
 			Setup(createPaasWithCondFn(paasWithCapability6, paasSpec, api.TypeHasErrorsPaas)).
 			Assess("is created", assertCap6NoUnwantedArtifacts).
 			Assess("is deleted when PaaS is deleted", assertCap6Deleted).
@@ -46,7 +65,6 @@ func TestCapabilityCap6(t *testing.T) {
 func assertCap6NoUnwantedArtifacts(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 	paas := getPaas(ctx, paasWithCapability6, t, cfg)
 	failWhenExists(ctx, cap6ApplicationSet, applicationSetNamespace, &argo.ApplicationSet{}, t, cfg)
-	failWhenExists(ctx, paasCap6Ns, cfg.Namespace(), &corev1.Namespace{}, t, cfg)
 	failWhenExists(ctx, paasCap6CRQ, cfg.Namespace(), &quotav1.ClusterResourceQuota{}, t, cfg)
 
 	// Paas has correct name
