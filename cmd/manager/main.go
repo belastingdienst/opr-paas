0e37a416de521b2dc48910e49ff1831bc7e3c641
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-02-13 16:06:03 +0100

    feat: paasns webhook (#327)
    * implementing PaasNSCustomValidator class and tests
    
    * Removing e2e tests that have moved to webhook tests

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -125,6 +125,9 @@ func main() {
 		if err = webhookv1alpha1.SetupPaasConfigWebhookWithManager(mgr); err != nil {
 			log.Fatal().Err(err).Str("webhook", "PaasConfig").Msg("unable to create webhook")
 		}
+		if err = webhookv1alpha1.SetupPaasNsWebhookWithManager(mgr); err != nil {
+			log.Fatal().Err(err).Str("webhook", "PaasNS").Msg("unable to create webhook")
+		}
 	}
 	if err = (&controller.PaasNSReconciler{
 		Client: mgr.GetClient(),
