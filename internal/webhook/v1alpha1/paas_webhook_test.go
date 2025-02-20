8828ab86c65b409be689b1de721402230bb0a422
AxiomaticFixedChimpanzee <179703613+AxiomaticFixedChimpanzee@users.noreply.github.com>
2025-02-20 07:23:37 +0100

    feat(paas_webhook): warn if quota requests higher than limits (#382)
    * feat(paas_webhook): warn if quota requests higher than limits
    
    * feat(paas_webhook): warn when extra permissions are requested but not configured

--- internal/webhook/v1alpha1/paas_webhook_test.go
+++ internal/webhook/v1alpha1/paas_webhook_test.go
@@ -15,9 +15,12 @@ import (
 
 	"github.com/belastingdienst/opr-paas/api/v1alpha1"
 	"github.com/belastingdienst/opr-paas/internal/config"
+	"github.com/belastingdienst/opr-paas/internal/quota"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -247,6 +250,66 @@ var _ = Describe("Paas Webhook", func() {
 
 			warnings, _ := validator.ValidateCreate(ctx, obj)
 			Expect(warnings).To(BeEmpty())
+		})
+
+		It("Should warn when quota limits are set higher than requests", func() {
+			conf := config.GetConfig()
+			conf.Capabilities["foo"] = v1alpha1.ConfigCapability{}
+			config.SetConfig(v1alpha1.PaasConfig{Spec: conf})
+
+			obj = &v1alpha1.Paas{
+				Spec: v1alpha1.PaasSpec{
+					Capabilities: v1alpha1.PaasCapabilities{
+						"foo": v1alpha1.PaasCapability{
+							Quota: quota.Quota{
+								corev1.ResourceLimitsCPU:      resource.MustParse("2"),
+								corev1.ResourceRequestsCPU:    resource.MustParse("2"),
+								corev1.ResourceLimitsMemory:   resource.MustParse("256Mi"),
+								corev1.ResourceRequestsMemory: resource.MustParse("1Gi"),
+							},
+						},
+					},
+					Quota: quota.Quota{
+						corev1.ResourceLimitsCPU:   resource.MustParse("10"),
+						corev1.ResourceRequestsCPU: resource.MustParse("11"),
+					},
+				},
+			}
+
+			warnings, err := validator.ValidateCreate(ctx, obj)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(warnings).To(HaveLen(2))
+			Expect(warnings).To(ContainElements(
+				"spec.quota CPU resource request (11) higher than limit (10)",
+				"spec.capabilities[foo].quota memory resource request (1Gi) higher than limit (256Mi)",
+			))
+		})
+
+		It("Should warn when extra permissions are requested for a capability that are not configured", func() {
+			conf := config.GetConfig()
+			conf.Capabilities["foo"] = v1alpha1.ConfigCapability{
+				ExtraPermissions: v1alpha1.ConfigCapPerm{
+					"bar": []string{"baz"},
+				},
+			}
+			conf.Capabilities["bar"] = v1alpha1.ConfigCapability{
+				// No extra permissions
+			}
+			config.SetConfig(v1alpha1.PaasConfig{Spec: conf})
+
+			obj = &v1alpha1.Paas{
+				Spec: v1alpha1.PaasSpec{
+					Capabilities: v1alpha1.PaasCapabilities{
+						"foo": v1alpha1.PaasCapability{ExtraPermissions: true},
+						"bar": v1alpha1.PaasCapability{ExtraPermissions: true},
+					},
+				},
+			}
+
+			warnings, err := validator.ValidateCreate(ctx, obj)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(warnings).To(HaveLen(1))
+			Expect(warnings[0]).To(Equal("spec.capabilities[bar].extra_permissions capability does not have extra permissions configured"))
 		})
 	})
 
