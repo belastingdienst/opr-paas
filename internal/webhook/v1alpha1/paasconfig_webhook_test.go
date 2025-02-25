d2ad6af9ec5ec297b9300bd4b6577aa24f6e816f
Hikarukin <156204784+hikarukin@users.noreply.github.com>
2025-02-25 17:11:03 +0100

    ci(tests): add more tests for PaasConfig validating webhook (#356)
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * ensure ClusterWideArgoCDNamespace is correctly marked as deprecated
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * undo accidental test commit
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * undo accidental test commit
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * add linter exception
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * ci(test): correct e2e test to use valid ldap hostname format
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * remove unneeded clusterwide check
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * wip
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * Correct naming for some functions
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * remove unused lines
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * add *.tar to .gitignore
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * remove unneeded cap6 test
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * fix invalid config
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * fix: remove hardcoded invalid response
    
    also correct / invert quota value check
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    * add TODO
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: hikarukin <156204784+hikarukin@users.noreply.github.com>

--- internal/webhook/v1alpha1/paasconfig_webhook_test.go
+++ internal/webhook/v1alpha1/paasconfig_webhook_test.go
@@ -6,16 +6,24 @@ See LICENSE.md for details.
 
 package v1alpha1
 
+// Excuse Ginkgo use from revive errors
+//revive:disable:dot-imports
+
 import (
+	"errors"
+	"fmt"
+
 	"github.com/belastingdienst/opr-paas/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-var _ = Describe("PaasConfig Webhook", func() {
+var _ = Describe("Creating a PaasConfig", func() {
 	var (
 		obj       *v1alpha1.PaasConfig
 		oldObj    *v1alpha1.PaasConfig
@@ -25,17 +33,18 @@ var _ = Describe("PaasConfig Webhook", func() {
 	)
 
 	BeforeEach(func() {
-		scheme = runtime.NewScheme()
-		Expect(v1alpha1.AddToScheme(scheme)).To(Succeed())
-
-		// Create a fake client that already has the existing PaasConfig
-		cl = fake.NewClientBuilder().
-			WithScheme(scheme).
-			Build()
-
-		obj = &v1alpha1.PaasConfig{}
+		obj = &v1alpha1.PaasConfig{
+			ObjectMeta: metav1.ObjectMeta{Name: "newPaasConfig"},
+			Spec: v1alpha1.PaasConfigSpec{
+				LDAP: v1alpha1.ConfigLdap{
+					Host: "some-invalid-hostname",
+					Port: 3309,
+				},
+				ExcludeAppSetName: "Something something",
+			},
+		}
 		oldObj = &v1alpha1.PaasConfig{}
-		validator = PaasConfigCustomValidator{client: cl}
+		validator = PaasConfigCustomValidator{client: k8sClient}
 		Expect(validator).NotTo(BeNil(), "Expected validator to be initialized")
 		Expect(oldObj).NotTo(BeNil(), "Expected oldObj to be initialized")
 		Expect(obj).NotTo(BeNil(), "Expected obj to be initialized")
@@ -58,24 +67,45 @@ var _ = Describe("PaasConfig Webhook", func() {
 					Build()
 
 				validator = PaasConfigCustomValidator{client: cl}
-				obj = &v1alpha1.PaasConfig{}
+				obj = &v1alpha1.PaasConfig{
+					ObjectMeta: metav1.ObjectMeta{Name: "newPaasConfig"},
+				}
 
 				warn, err := validator.ValidateCreate(ctx, obj)
 				Expect(warn, err).Error().To(HaveOccurred())
-				Expect(err.Error()).To(Equal("[]: Forbidden: another PaasConfig resource already exists"))
+				Expect(err.Error()).To(Equal("PaasConfig.cpet.belastingdienst.nl \"newPaasConfig\" is invalid: spec: Forbidden: another PaasConfig resource already exists"))
 			})
 		})
 
-		//It("Should admit creation if all required fields are present", func() {
-		//	By("simulating an invalid creation scenario")
-		//	Expect(validator.ValidateCreate(ctx, obj)).To(BeNil())
-		//})
-		//
-		// It("Should validate updates correctly", func() {
-		//     By("simulating a valid update scenario")
-		//     oldObj.SomeRequiredField = "updated_value"
-		//     obj.SomeRequiredField = "updated_value"
-		//     Expect(validator.ValidateUpdate(ctx, oldObj, obj)).To(BeNil())
-		// })
+		Context("and no PaasConfig already exists", func() {
+			Context("and the new PaasConfig does not have one or more required fields", func() {
+				It("should deny creation", func() {
+					warn, err := validator.ValidateCreate(ctx, obj)
+					fmt.Printf("DEBUG - %v", warn)
+					Expect(err).Error().To(HaveOccurred())
+					Expect(warn).To(HaveLen(1))
+					Expect(warn[0]).To(Equal("spec.excludeappsetname: deprecated"))
+
+					var serr *apierrors.StatusError
+					Expect(errors.As(err, &serr)).To(BeTrue())
+
+					causes := serr.Status().Details.Causes
+					Expect(causes).To(HaveLen(2))
+					expectedErrors := []metav1.StatusCause{
+						{
+							Type:    "FieldValueInvalid",
+							Message: "Invalid value: \"some-invalid-hostname\": invalid host name / ip address",
+							Field:   "spec.LDAP",
+						},
+						{
+							Type:    "FieldValueRequired",
+							Message: "Required value: DecryptKeysSecret is required and must have both name and namespace",
+							Field:   "spec.decryptkeyssecret",
+						},
+					}
+					Expect(causes).To(ConsistOf(expectedErrors))
+				})
+			})
+		})
 	})
 })
