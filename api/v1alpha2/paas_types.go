8cca84a7a4d7fe104ba51c7ba4dffb37dc7d7d90
AxiomaticFixedChimpanzee <179703613+AxiomaticFixedChimpanzee@users.noreply.github.com>
2025-03-31 12:05:36 +0200

    feat(api): define fields of v1alpha2 Paas version

--- api/v1alpha2/paas_types.go
+++ api/v1alpha2/paas_types.go
@@ -7,25 +7,100 @@ See LICENSE.md for details.
 package v1alpha2
 
 import (
+	paasquota "github.com/belastingdienst/opr-paas/internal/quota"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// Definitions to manage status conditions
+const (
+	// TypeReadyPaas represents the status of the Paas reconciliation
+	TypeReadyPaas = "Ready"
+	// TypeHasErrorsPaas represents the status used when the Paas reconciliation holds errors.
+	TypeHasErrorsPaas = "HasErrors"
+	// TypeDegradedPaas represents the status used when the Paas is deleted and the finalizer operations are yet to occur.
+	TypeDegradedPaas = "Degraded"
+)
 
 // PaasSpec defines the desired state of Paas
 type PaasSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Requestor is an informational field which decides on the requestor (also application responsible)
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:Required
+	Requestor string `json:"requestor"`
 
-	// Foo is an example field of Paas. Edit paas_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Quota defines the quotas which should be set on the cluster resource quota as used by this Paas project
+	// +kubebuilder:validation:Required
+	Quota paasquota.Quota `json:"quota"`
+
+	// Capabilities is a subset of capabilities that will be available in this Paas Project
+	// +kubebuilder:validation:Optional
+	Capabilities PaasCapabilities `json:"capabilities"`
+
+	// Groups define k8s groups, based on an LDAP query or a list of LDAP users, which get access to the namespaces
+	// belonging to this Paas. Per group, RBAC roles can be defined.
+	// +kubebuilder:validation:Optional
+	Groups PaasGroups `json:"groups"`
+
+	// Namespaces can be used to define extra namespaces to be created as part of this Paas project
+	// +kubebuilder:validation:Optional
+	Namespaces PaasNamespaces `json:"namespaces"`
+
+	// Secretes must be encrypted with a public key, for which the private key should be added to the DecryptKeySecret
+	// +kubebuilder:validation:Optional
+	Secrets map[string]string `json:"secrets"`
+
+	// Indicated by which 3rd party Paas this Paas is managed
+	// +kubebuilder:validation:Optional
+	ManagedByPaas string `json:"managedByPaas"`
+}
+
+type PaasCapability struct {
+	// Custom fields to configure this specific Capability
+	// +kubebuilder:validation:Optional
+	CustomFields map[string]string `json:"custom_fields"`
+	// This project has its own ClusterResourceQuota settings
+	// +kubebuilder:validation:Optional
+	Quota paasquota.Quota `json:"quota"`
+	// Secrets must be encrypted with a public key, for which the private key should be added to the DecryptKeySecret
+	// +kubebuilder:validation:Optional
+	Secrets map[string]string `json:"secrets"`
+	// You can enable extra permissions for the service accounts belonging to this capability
+	// Exact definitions is configured in Paas Configmap
+	// +kubebuilder:validation:Optional
+	ExtraPermissions bool `json:"extra_permissions"`
+}
+
+type PaasCapabilities map[string]PaasCapability
+
+type PaasGroup struct {
+	// A fully qualified LDAP query which will be used by the Group Sync Operator to sync users to the defined group.
+	//
+	// When set in combination with `users`, the Group Sync Operator will overwrite the manually assigned users.
+	// Therefore, this field is mutually exclusive with `group.users`.
+	// +kubebuilder:validation:Optional
+	Query string `json:"query"`
+	// A list of LDAP users which are added to the defined group.
+	//
+	// When set in combination with `users`, the Group Sync Operator will overwrite the manually assigned users.
+	// Therefore, this field is mutually exclusive with `group.query`.
+	// +kubebuilder:validation:Optional
+	Users []string `json:"users"`
+	// List of roles, as defined in the `PaasConfig` which the users in this group get assigned via a rolebinding.
+	// +kubebuilder:validation:Optional
+	Roles []string `json:"roles"`
+}
+
+type PaasGroups map[string]PaasGroup
+
+type PaasNamespaces map[string]PaasNamespace
+
+type PaasNamespace struct {
 }
 
 // PaasStatus defines the observed state of Paas
 type PaasStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:Optional
+	Conditions []metav1.Condition `json:"conditions" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
 // +kubebuilder:object:root=true
