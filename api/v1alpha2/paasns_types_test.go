8bbd32b85728720689c9bb0580d1934bd2b82e9a
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-04-14 13:10:15 +0200

    Adding paasns_tests

---
+++ api/v1alpha2/paasns_types_test.go
@@ -0,0 +1,12 @@
+package v1alpha2_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/belastingdienst/opr-paas/api/v1alpha2"
+)
+
+var _ = Describe("PaasnsTypes", func() {
+
+})
