a14a838263439fbbe6245fb4621948a5dd293981
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-04-30 12:34:20 +0200

    api/v1alpha2/paasns_types_test.go changes

--- api/v1alpha2/paasns_types_test.go
+++ api/v1alpha2/paasns_types_test.go
@@ -3,16 +3,58 @@ package v1alpha2
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("PaasnsTypes", func() {
+	const (
+		paasName   = "mypaas"
+		paasNsName = "mypaasns"
+		nsName     = paasName + "-" + paasNsName
+	)
 	var (
-		paasns PaasNS
+		paasNsLables = map[string]string{
+			instanceLabel: paasName,
+			"some-label":  "some-value",
+			"other-label": "other-value",
+		}
+		paasns = PaasNS{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      paasNsName,
+				Namespace: paasName,
+				Labels:    paasNsLables,
+			},
+		}
+		ownedObject = corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: nsName,
+				OwnerReferences: []metav1.OwnerReference{
+					{APIVersion: GroupVersion.Identifier(), Kind: "PaasNS", Name: paasNsName, Namespace: paasName},
+				},
+			},
+		}
 	)
 
+	BeforeAll(func() {
+	})
 	When("cloning labels", func() {
-		Context("on a paasns with ", Ordered, func() {
-			It("should succeed", func() {
+		Context("on a paasns with labels", Ordered, func() {
+			labels := paasns.ClonedLabels()
+			It("should not return the "+instanceLabel+" label", func() {
+				Expect(paasNsLables).To(HaveKey(instanceLabel))
+				Expect(labels).NotTo(HaveKey(instanceLabel))
+			})
+			It("should return all other labels", func() {
+				for key, value := range paasNsLables {
+					Expect(labels).To(HaveKeyWithValue(key, value))
+				}
+			})
+		})
+	})
+	When("checking paasns ownership", func() {
+		Context("on a resource", Ordered, func() {
+			It("should return when ownership is properly configured", func() {
 			})
 		})
 	})
