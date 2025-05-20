657b66b6fb586054a2822ac94e6af45c89778504
portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
2025-05-20 08:51:07 +0200

    Fix e2e tests
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>

--- test/e2e/groupusers_test.go
+++ test/e2e/groupusers_test.go
@@ -62,7 +62,7 @@ func assertGroupCreated(ctx context.Context, t *testing.T, cfg *envconf.Config) 
 	assert.Equal(t, "[foo]", group.Users.String())
 	// Correct labels are defined
 	assert.Len(t, group.Labels, 1)
-	assert.Equal(t, "paas", group.Labels["app.kubernetes.io/managed-by"], "Labeled as managed by Paas")
+	assert.Equal(t, paas.Name, group.Labels["app.kubernetes.io/managed-by"], "Labeled as managed by Paas")
 	assert.Empty(t, group.Annotations, "Group should have no annotations")
 	// The owner of the group is the Paas that created it
 	assert.Equal(t, paas.UID, group.OwnerReferences[0].UID)
@@ -164,7 +164,7 @@ func assertOldGroupRemovedAfterUpdatingKey(ctx context.Context, t *testing.T, cf
 	assert.NotContains(t, controller.LdapHostLabelKey, updatedGroup2.Labels)
 	assert.Equal(
 		t,
-		paas.Name,
+		paasWithGroups,
 		updatedGroup2.Labels[controller.ManagedByLabelKey],
 		"Labeled as managed by Paas.name",
 	)
