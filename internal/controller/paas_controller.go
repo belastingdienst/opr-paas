e5f28a4192fc05d0dad2cd044320ac504d23f420
portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
2025-05-23 09:06:07 +0200

    Removes predicate in Watched PaasNs, as that predicate is unrelated.
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>

--- internal/controller/paas_controller.go
+++ internal/controller/paas_controller.go
@@ -325,7 +325,7 @@ func allPaases(mgr ctrl.Manager) []reconcile.Request {
 }
 
 // SetupWithManager sets up the controller with the Manager.
-// SetupWithManager is not unittesterd ATM. Mostly already covered by e2e tests.
+// SetupWithManager is not unit-tested ATM. Mostly covered by e2e-tests.
 func (r *PaasReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Paas{}, builder.WithPredicates(
@@ -358,9 +358,7 @@ func (r *PaasReconciler) SetupWithManager(mgr ctrl.Manager) error {
 					return []reconcile.Request{{
 						NamespacedName: types.NamespacedName{Name: paasName}}}
 				},
-			),
-			builder.WithPredicates(v1alpha1.ActivePaasConfigUpdated()),
-		).
+			)).
 		Watches(
 			&v1alpha1.PaasConfig{},
 			handler.EnqueueRequestsFromMapFunc(func(_ context.Context, _ client.Object) []reconcile.Request {
