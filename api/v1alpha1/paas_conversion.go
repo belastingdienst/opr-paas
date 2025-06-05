bd5503634cb8a16ffb2bdd7eb166e69e84a44969
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-06-05 12:17:44 +0200

    Having both custom fields and obsolete fields in v1alpha1 is probably best (e.a. to prevent ArgoCD missing fields that it is managing).

--- api/v1alpha1/paas_conversion.go
+++ api/v1alpha1/paas_conversion.go
@@ -46,9 +46,15 @@ func (p *Paas) ConvertFrom(srcRaw conversion.Hub) error {
 
 	for name, capability := range src.Spec.Capabilities {
 		fields := capability.DeepCopy().CustomFields
+		gitUrl := fields[gitUrlKey]
+		gitRevision := fields[gitRevisionKey]
+		gitPath := fields[gitPathKey]
 
 		p.Spec.Capabilities[name] = PaasCapability{
 			Enabled:          true,
+			GitURL:           gitUrl,
+			GitRevision:      gitRevision,
+			GitPath:          gitPath,
 			CustomFields:     fields,
 			Quota:            capability.Quota,
 			SSHSecrets:       capability.Secrets,
